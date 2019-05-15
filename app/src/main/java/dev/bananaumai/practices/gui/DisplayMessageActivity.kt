2069afc542e5e59abc427666e707cc43c36cc12e
YUTA SHIMAKAWA <banana.umai@gmail.com>
2019-05-15 15:10:49 -0700

    debug activity lifecycle

--- app/src/main/java/dev/bananaumai/practices/gui/DisplayMessageActivity.kt
+++ app/src/main/java/dev/bananaumai/practices/gui/DisplayMessageActivity.kt
@@ -2,18 +2,58 @@ package dev.bananaumai.practices.gui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 
 class DisplayMessageActivity : AppCompatActivity() {
+    private val tag = this::class.java.name
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
 
+        Log.d(tag, "onCreate() : is finishing? -> $isFinishing")
+
         intent.getStringExtra(MainActivity.EXTRA_MESSAGE).let {
             val textView = findViewById<TextView>(R.id.textView)
             textView.text = "${textView.text}  $it"
         }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.d(tag, "onRestart() : is finishing? -> $isFinishing")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d(tag, "onStart() : is finishing? -> $isFinishing")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d(tag, "onResume() : is finishing? -> $isFinishing")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(tag, "onPause() : is finishing? -> $isFinishing")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(tag, "onStop() : is finishing? -> $isFinishing")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(tag, "onDestroy() : is finishing? -> $isFinishing")
 
     }
 }
--- app/src/main/java/dev/bananaumai/practices/gui/MainActivity.kt
+++ app/src/main/java/dev/bananaumai/practices/gui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.util.Log
 import android.view.View
 import android.widget.EditText
 
@@ -12,9 +13,50 @@ class MainActivity : AppCompatActivity() {
         const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
     }
 
+    private val tag = this::class.java.name
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d(tag, "onCreate() : is finishing? -> $isFinishing")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.d(tag, "onRestart() : is finishing? -> $isFinishing")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.d(tag, "onStart() : is finishing? -> $isFinishing")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d(tag, "onResume() : is finishing? -> $isFinishing")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d(tag, "onPause() : is finishing? -> $isFinishing")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d(tag, "onStop() : is finishing? -> $isFinishing")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d(tag, "onDestroy() : is finishing? -> $isFinishing")
+
     }
 
     fun sendMessage(view: View) {
